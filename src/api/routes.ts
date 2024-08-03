491d3eea06427a39289f7443353148468dcf7046
Mayank Chaturvedi <mayankchaturvedi@Mayanks-MacBook-Air.local>
2024-08-03 15:15:45 +0530

    Cards API

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -1,7 +1,12 @@
 import * as express from 'express';
+const cardList = require('../content/cards.json');
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-    res.send('hello from route')
-})
+router.get('/cards', async (req, res) => {
+  res.send(cardList);
+});
+
+router.post('/chat', async (req, res) => {
+  res.send('hello from route');
+});
 export default router;
